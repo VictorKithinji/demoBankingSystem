88946794b5954735f1cc8658782ace5309cee991
MarkKithinji <105651925+MarkKithinji@users.noreply.github.com>
2022-10-31 13:19:51 +0300

    Add project files.

---
+++ ObjectsANDClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjectsANDClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO SUPERBANK!");
+            //!instructions for the Bank
+            
+
+            //!these are the Characteristics of the bank (its properties)
+            //it has a ten digit number that identifies the Bank
+            //it has a string that storesthe names of owners
+
+            //! these are the methods
+            // the balance can be retrieved
+            //it accepts deposites
+
+            //!these are sort of rules to consider when implemeting the methods
+            //Accepts withdrawalsThe initial balance must be positive
+            // withdrawals cannot accept a negative balance
+
+
+        }
+    }
+}
